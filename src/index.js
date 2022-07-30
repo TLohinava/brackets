d9ce4e7eda060b2aab5296cf3d35e1252c76048a
TLohinava <lohinava.t@gmail.com>
2022-07-30 14:06:40 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = bracketsConfig.map(item => item.join(''));
+  if(str.length%2 === 0 ) {
+    for (let i = str.length-1; i >=0; i--) {
+      for (let j = 0; j < arr.length; j++) {
+        if(str.includes(arr[j])) {
+          str = str.replaceAll(arr[j], "");
+        }
+      } 
+    }
+  } return str.length === 0;
 }
